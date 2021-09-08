5e7e35986dd45b14253b01b25d606193fd4dbc45
ShakalakaB <yunjie989@gmail.com>
2021-09-08 18:43:58 +0800

    Edit print msg

--- src/main/java/com/awesome/wow/designpattern/MyMethodInterceptor.java
+++ src/main/java/com/awesome/wow/designpattern/MyMethodInterceptor.java
@@ -8,9 +8,9 @@ import java.lang.reflect.Method;
 public class MyMethodInterceptor implements MethodInterceptor {
     @Override
     public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
-        System.out.println("before method: " + method.getName());
+        System.out.println("intercept before method: " + method.getName());
         Object object = methodProxy.invokeSuper(o, args);
-        System.out.println("after method: " + method.getName());
+        System.out.println("intercept after method: " + method.getName());
         return object;
     }
 }
