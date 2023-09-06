6df818c31432a3c2ac3efdfd7d5a3a03c1fa2cc8
Aldora <aldora988@gmail.com>
2023-09-05 22:05:08 -0700

    ArrayList assignment for Georgia Tech edX

--- src/main/java/com/awesome/wow/Test.java
+++ src/main/java/com/awesome/wow/Test.java
@@ -1,5 +1,7 @@
 package com.awesome.wow;
 
+import com.awesome.wow.gt.ArrayList;
+
 import java.util.*;
 import java.util.List;
 
@@ -14,6 +16,17 @@ public class Test {
         Car car4 = Car.builder().keyA(2).keyB(2).name("car2").build();
         Car car5 = Car.builder().keyA(2).keyB(3).name("car2").build();
         List<Car> carList = Arrays.asList(car1, car2, car3, car4, car5);
+
+//        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 8};
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.addToBack(arr[i]);
+        }
+        System.out.println(list.removeFromFront());
+        System.out.println(list.removeFromBack());
+        System.out.println(list.removeFromBack());
+        System.out.println(list);
 
     }
 
