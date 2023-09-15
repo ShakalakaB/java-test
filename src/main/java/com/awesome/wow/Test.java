c763c925d50a687384c8b22ae50145c2b662d5de
Aldora <aldora988@gmail.com>
2023-09-14 19:48:56 -0700

    Assignment ArrayQueue

--- src/main/java/com/awesome/wow/Test.java
+++ src/main/java/com/awesome/wow/Test.java
@@ -1,6 +1,6 @@
 package com.awesome.wow;
 
-import com.awesome.wow.gt.ArrayList;
+import com.awesome.wow.gt.ArrayQueue;
 import com.awesome.wow.gt.SinglyLinkedList;
 import com.awesome.wow.gt.SinglyLinkedListNode;
 
@@ -18,28 +18,32 @@ public class Test {
         Car car4 = Car.builder().keyA(2).keyB(2).name("car2").build();
         Car car5 = Car.builder().keyA(2).keyB(3).name("car2").build();
         List<Car> carList = Arrays.asList(car1, car2, car3, car4, car5);
-
-        Integer[] arr = {1, 2, 3};
+        
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 //        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
         for (int i = 0; i < arr.length; i++) {
-            list.addToBack(arr[i]);
+            queue.enqueue(arr[i]);
         }
-        System.out.println(list.removeFromFront());
-        System.out.println(list.removeFromBack());
-        System.out.println(list.removeFromBack());
-        System.out.println(list.removeFromBack());
-        printList(list);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        queue.enqueue(10);
+        queue.enqueue(11);
+        queue.enqueue(12);
+        queue.enqueue(13);
+        printList(queue);
 
     }
 
-    private static void printList(SinglyLinkedList<Integer> list) {
+    private static void printList(ArrayQueue<Integer> queue) {
         StringBuilder output = new StringBuilder();
-        SinglyLinkedListNode<Integer> current = list.getHead();
-        while (current != null) {
-            output.append(current.getData()).append(", ");
-            current = current.getNext();
-        }
+//        SinglyLinkedListNode<Integer> current = list.getHead();
+//        while (current != null) {
+//            output.append(current.getData()).append(", ");
+//            current = current.getNext();
+//        }
         System.out.println(output);
     }
 
